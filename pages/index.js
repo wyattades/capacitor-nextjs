dc010ff8bca18fe92bf649c28572de39710e67b2
Wyatt Ades <wyattades@gmail.com>
2021-05-26 17:46:29 -0700

    fix duplicate script loading

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Link from "components/Link";
 import Layout from "components/Layout";
@@ -7,35 +7,6 @@ const IndexPage = () => {
   return (
     <Layout>
       <h1>Index page</h1>
-
-      <div>
-        <span
-          onClick={() => {
-            alert("hi div");
-          }}
-        >
-          Test span onClick
-        </span>
-        <br />
-        <br />
-        <button
-          onClick={() => {
-            alert("hi div");
-          }}
-        >
-          Test button onClick
-        </button>
-        <br />
-        <br />
-        <a
-          onClick={(e) => {
-            e.preventDefault();
-            alert("hi anchor");
-          }}
-        >
-          Test anchor onClick
-        </a>
-      </div>
     </Layout>
   );
 };
